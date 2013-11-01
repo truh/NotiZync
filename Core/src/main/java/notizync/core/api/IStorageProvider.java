1f4c290bfd31db25702c791179591a2d77918f40
Jakob Klepp <jklepp@student.tgm.ac.at>
2013-11-01 13:24:08 -0400

    javadoc'd IStorageProvider

--- Core/src/main/java/notizync/core/api/IStorageProvider.java
+++ Core/src/main/java/notizync/core/api/IStorageProvider.java
@@ -8,26 +8,23 @@ import java.util.Set;
 public interface IStorageProvider {
 
     /**
+     * Adds or updates a note for storage.
      *
-     * @param note
-     * @return
+     * @param note note that should be stored
+     * @return note that was stored before with the same title or null
      */
     public INote putNote(INote note);
 
     /**
-     *
-     * @return
+     * @return set of notes stored by this StorageProvider
      */
     public Set<? extends INote> getNoteSet();
 
     /**
+     * Writes changes to destination
+     * Reads updates from source
      *
+     * Might do nothing if StorageProvider works unbuffered.
      */
     public void sync();
-
-    /**
-     *
-     * @return
-     */
-    public int getUniqueID();
 }
